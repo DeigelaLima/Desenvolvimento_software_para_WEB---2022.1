37b746c6ca63174a87f86bfd09fa622a4cad6b43
Deigela Lima <deigelalima@hotmail.com>
2022-06-26 20:31:34 -0300

    Resolucao da lista_5

--- Lista 05 - Formulário e validação - Login & Cadastro/signup.js
+++ Lista 05 - Formulário e validação - Login & Cadastro/signup.js
@@ -50,49 +50,109 @@ function popularDias(mes){
     }
   }
 
-  // Insere os dias corretos na seleção de dia <select>
-  for(let i = 1; i <= diaNum; i++){
-    const option = document.createElement("option");
-    option.textContent = i;
-    diaSelect.appendChild(option);
-  }
-  if(diaAnterior){
-    diaSelect.value = diaAnterior;
-    if(diaSelect.value === ""){
-      diaSelect.value = diaAnterior - 1;
+    // Insere os dias corretos na seleção de dia <select>
+    for(let i = 1; i <= diaNum; i++){
+      const option = document.createElement("option");
+      option.textContent = i;
+      diaSelect.appendChild(option);
     }
-    if(diaSelect.value === ""){
-      diaSelect.value = diaAnterior - 2;
-    }
-    if(diaSelect.value === ""){
-      diaSelect.value = diaAnterior - 3;
+    if(diaAnterior){
+      diaSelect.value = diaAnterior;
+      if(diaSelect.value === ""){
+        diaSelect.value = diaAnterior - 1;
+      }
+      if(diaSelect.value === ""){
+        diaSelect.value = diaAnterior - 2;
+      }
+      if(diaSelect.value === ""){
+        diaSelect.value = diaAnterior - 3;
+      }
     }
   }
-}
 
-function popularAnos(){
-  // Obter o ano atual como um número.
-  let ano = new Date().getFullYear();
-  // Faz com que os 100 anos anteriores sejam uma opção.
-  for(let i = 0; i < 51; i++){
-    const option = document.createElement("option");
-    option.textContent = ano - i;
-    anoSelect.appendChild(option);
+  function popularAnos(){
+    // Obter o ano atual como um número.
+    let ano = new Date().getFullYear();
+    // Faz com que os 100 anos anteriores sejam uma opção.
+    for(let i = 0; i < 21; i++){
+      const option = document.createElement("option");
+      option.textContent = ano - i;
+      anoSelect.appendChild(option);
+    }
   }
-}
 
-popularDias(mesSelect.value);
-popularAnos();
-
-anoSelect.onchange = function(){
   popularDias(mesSelect.value);
-}
-mesSelect.onchange = function(){
-  popularDias(mesSelect.value);
-}
-diaSelect.onchange = function(){
-  diaAnterior = diaSelect.value;
-}
+  popularAnos();
+
+  anoSelect.onchange = function(){
+    popularDias(mesSelect.value);
+  }
+  mesSelect.onchange = function(){
+    popularDias(mesSelect.value);
+  }
+  diaSelect.onchange = function(){
+    diaAnterior = diaSelect.value;
+  }
+
+// // Função para validar os campos do formulário
+// function validar(){
+//   var nome = document.getElementById('nome').value;
+//   var email = document.getElementById('email').value;
+//   var senha = document.getElementById('senha').value;
+//   var confirmaSenha = document.getElementById('confirma-senha').value;
+
+//   // validação do nome
+//   if(nome == ""){
+//     document.getElementById('erro-nome').innerHTML = "Por favor, informe um nome de usuário.";
+//     document.getElementById('nome').focus();
+//     return false;
+//   }
+//   else{
+//     // Se o nome tiver preenchido o alerta(vermelho) some.
+//     document.getElementById('erro-nome').innerHTML = "";
+//   }
+
+//   // validação do email
+//   if(email == ""){
+//     document.getElementById('erro-email').innerHTML = "Informe um email válido.";
+//     document.getElementById('email').focus();
+//     return false;
+//   }
+//   else{
+//     // Se o nome tiver preenchido o alerta(vermelho) some.
+//     document.getElementById('erro-email').innerHTML = "";
+//   }
+
+//   // validação da senha
+//   if(senha == ""){
+//     document.getElementById('erro-senha').innerHTML = "A senha é obrigatória e deve ter pelo menos 6 caracteres.";
+//     document.getElementById('senha').focus();
+//     return false;
+//   }
+//   else{
+//     // Se o nome tiver preenchido, o alerta(vermelho) some.
+//     document.getElementById('erro-senha').innerHTML = "";
+//     // validação do confirma-senha
+//     if(confirmaSenha == ""){
+//       document.getElementById('erro-confirma-senha').innerHTML = "A senha é obrigatória e deve ter pelo menos 6 caracteres.";
+//       document.getElementById('confirma-senha').focus();
+//       return false;
+//     }
+//     else{
+//       document.getElementById('erro-confirma-senha').innerHTML = "";
+//       // Verifica se as senha são iguais
+//       if(senha != confirmaSenha){
+//         document.getElementById('senhas-diferentes').removeAttribute('hidden'); 
+//       }
+//       else{
+//         // Se o nome tiver preenchido, o alerta(vermelho) some.
+//         document.getElementById('senhas-diferentes').setAttribute('hidden', 'true');
+
+//       }
+//     }
+//   }
+// }
+
 
 
 
